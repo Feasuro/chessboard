9da4215a4236b7a2f357085975d4493094b1ca77
Feasuro <feasuro@proton.me>
2022-11-09 20:21:31 +0100

    Initial commit

---
+++ chessboard-qtgui.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python
+
+import re
+import sys
+import time
+
+from PyQt6.QtCore import QPoint, QProcess, QRect, QSize, Qt
+from PyQt6.QtCore import pyqtSignal as Signal
+from PyQt6.QtCore import pyqtSlot as Slot
+from PyQt6.QtGui import QAction, QBrush, QIcon, QPainter
+from PyQt6.QtWidgets import (QAbstractButton, QApplication, QCheckBox,
+                             QDockWidget, QGridLayout, QInputDialog, QLabel,
+                             QListWidget, QListWidgetItem, QMainWindow,
+                             QMessageBox, QSizePolicy, QToolBar, QWidget)
+
+from chessboard import ChessBoard
+
+
+class Solution(QListWidgetItem):
+    """It is made from list not str and stores it for later use"""
+
+    def __init__(self, lst: list, *args, **kwargs):
+        super().__init__(str(lst), *args, **kwargs)
+
+        self.queens = lst[:]
+
+
+class FieldButton(QAbstractButton):
+    """Button that looks like a chessboard field"""
+    click = Signal(tuple)
+
+    def __init__(self, field, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        #fieldbutton is aware of it's position and color
+        self.col, self.row = field
+
+        if self.col % 2 * self.row % 2 == 0 and self.col % 2 + self.row % 2 == 1:
+            self.setProperty('color', 'white')
+        else:
+            self.setProperty('color', 'black')
+
+        #emits it's coordinates when clicked
+        self.clicked.connect(self.on_click)
+
+        #wants to be square
+        square = QSizePolicy()
+        square.setHeightForWidth(True)
+        square.setHorizontalPolicy(QSizePolicy.Policy.Expanding)
+        square.setVerticalPolicy(QSizePolicy.Policy.Ignored)
+        self.setSizePolicy(square)
+
+    def heightForWidth(self, width: int) -> int:
+        return width
+
+    def sizeHint(self):
+        return QSize(36, 36)
+
+    def paintEvent(self, event) -> None:
+        #how it actually looks
+        painter = QPainter(self)
+        if self.property('color') == 'white':
+            painter.setBrush(QBrush(Qt.GlobalColor.white))
+        elif self.property('color') == 'black':
+            painter.setBrush(QBrush(Qt.GlobalColor.black))
+        painter.drawRect(QRect(QPoint(0, 0), self.size()))
+        self.icon().paint(painter, int(self.width() / 6), int(self.height() / 6), int(self.width() * 2/3), int(self.height() * 2/3))
+
+    @Slot()
+    def on_click(self):
+        self.click.emit((self.col, self.row))
+
+
+class Board(QWidget):
+    """Widget that stores the chessboard and draws it"""
+
+    def __init__(self, dim, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        #here's the chessboard and it's fields graphic representation
+        self.chess = ChessBoard(dim)
+        self.buttons = {field: FieldButton(field, click=self.field_state) for field in self.chess.board}
+
+        #field-buttons arranged properly
+        layout = QGridLayout()
+        layout.setSpacing(0)
+        for i in range(self.chess.dim):
+            layout.addWidget(QLabel(str(i+1)+' '), self.chess.dim - i -1, 0)
+            layout.addWidget(QLabel(str(i+1)), self.chess.dim, i + 1, alignment=Qt.AlignmentFlag.AlignCenter)
+        for field in self.buttons:
+            layout.addWidget(self.buttons[field], self.chess.dim - field[1], field[0])
+        self.setLayout(layout)
+
+    def paintEvent(self, event) -> None:
+        #always be square 
+        self.setFixedWidth(self.height())
+        return super().paintEvent(event)
+
+    def field_state(self, field):
+        #updates chessboard's state when a field is clicked
+        if self.chess.place_queen(field):
+            self.update_state()
+        else:
+            QMessageBox.critical(self, 'Forbidden', 'Cannot place Queen here!')
+
+    def update_state(self):
+        #redraws icons after underlying chessboard's change
+        for field in self.chess.board:
+            if self.chess.board[field] == 'Q' and self.buttons[field].property('color') == 'white':
+                self.buttons[field].setIcon(QIcon('./resources/chess-queen-bt.png'))
+            elif self.chess.board[field] == 'Q' and self.buttons[field].property('color') == 'black':
+                self.buttons[field].setIcon(QIcon('./resources/chess-queen-wt.png'))
+            elif self.chess.board[field] == '+':
+                self.buttons[field].setIcon(QIcon('./resources/cross.png'))
+            else:
+                self.buttons[field].setIcon(QIcon())
+
+
+class MainWindow(QMainWindow):
+    """Provides graphic interface for playing with and solving the n-Queens problem"""
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+        #size and title bar
+        self.setWindowTitle('N-Queens solver')
+        self.setWindowIcon(QIcon('./resources/chess-queen-wt.png'))
+        self.setGeometry(100,100,700,500)
+
+        #setup control of worker proces for computation
+        self.solver = QProcess()
+        self.solver.setProgram('./chessboard.py')
+        self.solver.finished.connect(self.finish_computation)
+        self.solver.readyReadStandardOutput.connect(self.populate_solutions)
+        self.solver.readyReadStandardError.connect(self.handle_stderr)
+        self.solver.time = '?'
+
+        #make the window and show it
+        self.ui_setup()
+        self.show()
+
+    def ui_setup(self):
+        #put chessboard in the center
+        self.body = Board(0)
+        self.setCentralWidget(self.body)
+
+        #list of solutions in dock widget
+        self.solutions = QListWidget()
+        self.solutions.itemDoubleClicked.connect(self.show_solution)
+        dock = QDockWidget('Solutions')
+        dock.setAllowedAreas(Qt.DockWidgetArea.RightDockWidgetArea | Qt.DockWidgetArea.LeftDockWidgetArea)
+        dock.setFeatures(QDockWidget.DockWidgetFeature.DockWidgetMovable | QDockWidget.DockWidgetFeature.DockWidgetFloatable)
+        dock.setMaximumWidth(250)
+        dock.setWidget(self.solutions)
+        self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, dock)
+
+        #available actions creation and configuration
+        self.actions = []
+        for name in ('new', 'clean', 'solve', 'cancel'):
+            action = QAction(name.capitalize(), self)
+            action.setObjectName(name)
+            self.actions.append(action)
+        self.actions[0].triggered.connect(self.new_chessboard)
+        self.actions[1].triggered.connect(self.clean_chessboard)
+        self.actions[2].triggered.connect(self.solve_chessboard)
+        self.actions[3].triggered.connect(self.finish_computation)
+        self.actions[3].setDisabled(True)
+        self.multi = QCheckBox('Multiprocess solving', self)
+
+        #toolbar arrangement and styling
+        toolbar = QToolBar()
+        toolbar.addAction(self.actions[0])
+        toolbar.addAction(self.actions[1])
+        toolbar.addSeparator()
+        toolbar.addAction(self.actions[2])
+        toolbar.addWidget(self.multi)
+        toolbar.addAction(self.actions[3])
+        for a in self.actions:
+            toolbar.widgetForAction(a).setObjectName(a.objectName())
+        toolbar.setStyleSheet( " QToolButton#cancel::enabled { color: red; } " )
+        self.addToolBar(toolbar)
+
+        #statusbar setup
+        self.statusbar = self.statusBar()
+        self.statuslabel = QLabel('0 known solutions')
+        self.statusbar.addPermanentWidget(self.statuslabel)
+
+    def new_chessboard(self):
+        #ask user for dimension, clear the solution list and create new chessboard 
+        dim, ok = QInputDialog.getInt(self, 'New Chessboard', "Provide new chessboard's dimension:")
+        if ok :
+            self.solutions.clear()
+            self.body = Board(dim)
+            self.setCentralWidget(self.body)
+
+    def clean_chessboard(self):
+        #clear chessboard and update icons
+        self.body.chess.clean()
+        self.body.update_state()
+
+    def solve_chessboard(self):
+        #remove previous solutions
+        self.solutions.clear()
+        #change actions' availability when computations are in progress
+        self.actions[3].setDisabled(False)
+        for i in range(3):
+            self.actions[i].setDisabled(True)
+        self.multi.setDisabled(True)
+        #pass actual chessboard arrangement as commandline arguments
+        M = ['-m'] if self.multi.isChecked() else []
+        self.solver.setArguments(M + ['-d', str(self.body.chess.dim), '-q'] + [str(q) if q else '0' for q in self.body.chess.queens])
+        #give some feedback on what is being done
+        print(time.strftime('%x %X'), 'Starting solver process.')
+        self.statusbar.showMessage('Computation in progress')
+        #fire up the calculations
+        self.solver.start()
+
+    def finish_computation(self):
+        #if process is still running cancel it
+        if self.solver.state() is not QProcess.ProcessState.NotRunning :
+            print(time.strftime('%x %X'), 'Terminating solver process')
+            self.solver.terminate()
+        #print some feedback
+        print(time.strftime('%x %X'), 'Solver process stopped:', self.solver.exitStatus())
+        self.statusbar.showMessage('Finished', 4000)
+        #restore default actions' availability
+        self.multi.setDisabled(False)
+        for i in range(3):
+            self.actions[i].setDisabled(False)
+        self.actions[3].setDisabled(True)
+        #display a summary messages
+        self.statuslabel.setText(f'{self.solutions.count()} known solutions')
+        seconds = round(self.solver.time, 2) if isinstance(self.solver.time, float) else self.solver.time
+        QMessageBox.information(self, 'Solved', f'Found {self.solutions.count()} solutions in {seconds} seconds.')
+        self.solver.time = '?'
+
+    def populate_solutions(self):
+        #read results, reformat and append them to the list
+        while data := self.solver.readLine() :
+            text = str(data).strip("bn[']\\")
+            queens = [int(i) for i in text.split(', ')] if text else []
+            Solution(queens, self.solutions)
+
+    def show_solution(self, solution: Solution):
+        #maps given solution to the current chessboard
+        self.body.chess.clean()
+        for field in enumerate(solution.queens, start=1):
+            self.body.chess.place_queen(field)
+        self.body.update_state()
+
+    def handle_stderr(self):
+        #print worker stderr output to terminal, remember running time
+        regex = 'Elapsed time: (\d+.\d+e?-?\d*) seconds'
+        data = self.solver.readAllStandardError()
+        stderr = bytes(data).decode("utf8")
+        print('Debug:', regex, stderr)
+        self.solver.time = float( re.search(regex, stderr).group(1) )
+        print(stderr)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec())
---
+++ chessboard.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python
+
+import sys
+import time
+from concurrent.futures import ProcessPoolExecutor
+from copy import deepcopy
+
+
+class ChessBoard :
+    """ Class implements the N Queens problem.
+
+    Values of the 'board' attribute say if the field is:
+    'Q' - occupied,
+    '+' - threatened,
+    '_' - empty.
+    Methods (see examples below) allow user to manually look for solutions
+    by placing/removing queens on the chessboard
+    or print result in human readable format. """
+
+    def __init__(self, N: int, queens=None) :
+        """ Pass chessboard's dimension as argument 
+        
+        example: B=ChessBoard(8)
+        creates standard 8x8 chessboard named B"""
+        self._dim = int(N) if N > 0 else 0
+        self.board = dict( ((n, k), '_') for n in range(1, self.dim+1) for k in range(1, self.dim+1) )
+        self.queens = [None] * self.dim
+        if queens is not None :
+            self.from_list(queens)
+
+    @property
+    def dim(self) :
+        """ Chessboard's dimension """
+        return self._dim
+
+    def __bool__(self) -> bool:
+        return self.dim != 0
+
+    def __str__(self) -> str:
+        """ Use print() to view chessboard on the screen. 
+        
+        example: print(B)
+        view chessboard B"""
+        # Upper line
+        result = '    '
+        for n in range(self.dim) :
+            result += '_ '
+        result += '\n'
+        # Chessboard
+        for k in range(self.dim, 0, -1) :
+            result += f'{k:>2} |'
+            for n in range(1, self.dim+1) :
+                result += self.board[n, k] + '|'
+            result += '\n'
+        # Column numeration
+        result += '    '
+        for n in range(1, self.dim+1) :
+            result += f'{n:<2}'
+        result += '\n'
+        return result
+
+    def clean(self) :
+        """ Cleans the chessboard empty 
+        
+        example: B.clean()"""
+        for f in self.board :
+            self.board[f] = '_'
+        self.queens = [None] * self.dim
+        return 0
+
+    def get_field(self) :
+        """ Looks for an empty field to place a Queen. """
+        try :
+            return min(f for f in self.board if self.board[f] == '_')
+        except ValueError :
+            print('No more unthreatened fields on the chessboard!')
+            return None
+
+    def get_fields(self, col=None) :
+        """ Returns empty fields up to given column, all empty fields by default. """
+        if col is None : col = self.dim
+        return (f for f in self.board if f[0] <= col and self.board[f] == '_')
+
+    def place_queen(self, field) :
+        """ Place or remove a queen from given field. Marks appropriate fields as threatened. 
+        
+        example: B.place_queen((2,3))
+        puts (or deletes) queen in 2'nd column and 3'rd row of the chessboard B"""
+        if field in self.board :
+            if self.board[field] == '_' :
+                self.queens[field[0] - 1] = field[1]
+                self.board[field] = 'Q'
+                for f in self.board :
+                    if self.board[f] == '_' and check(f, field) :
+                        self.board[f] = '+'
+                return True
+            elif self.board[field] == 'Q' :
+                self.queens[field[0] - 1] = None
+                self.board[field] = '_'
+                for f in self.board :
+                    if self.board[f] == '+' and check(f, field) :
+                        self.board[f] = '_'
+                        for i in range(self.dim) :
+                            if self.queens[i] != None and check(f, (i+1, self.queens[i])) :
+                                self.board[f] = '+'
+                                break
+                return True
+            else :
+                print('Cannot place Queen here!')
+                return False
+        else :
+            raise ValueError('Incorrect field coordinates (out of range?)')
+
+    def from_list(self, queens):
+        """ Populates board's initial values when queens arrangement is given to the constructor """
+        #for field in zip( range(1, self.dim+1), queens, strict=True ) :
+        for field in enumerate(queens, start=1) :
+            if field[1] is None :
+                continue
+            if not self.place_queen(field) :
+                raise ValueError('Incorrect queens arrangement provided - attacking each other!)')
+
+    def solve(self) :
+        """ Yields all possible solutions """
+        try :
+            col = self.queens.index(None) + 1
+        except ValueError :
+            yield self.queens
+        else :
+            for f in self.get_fields(col) :
+                self.place_queen(f)
+                yield from self.solve()
+                self.place_queen(f)
+
+    def solutions(self, multi=False, verbose=False) :
+        """ Wrapper method for printing solutions.
+
+        This method picks solver fuction and output format:
+        if multi=True uses multiprocessing.
+        if verbose=True prints human readable visualisation of solutions
+    
+        example1: B.solutions()
+        Prints all solutions for chessboard B
+
+        example2: B.solutions(verbose=True)
+        The same as above but uses human readable output
+        
+        example3: B.solutions(multi=True)
+        Takes advantage of multiprocess concurrent calculations """
+
+        if verbose :
+            output = print
+        else :
+            output = alt_print
+
+        if multi :
+            solver = multi_solver
+        else :
+            solver = mono_solver
+
+        start = time.perf_counter()
+        solver(self, output)
+        stop = time.perf_counter()
+        print('Elapsed time:', stop - start, 'seconds', file=sys.stderr)
+
+
+def alt_print(chessboard: ChessBoard) :
+    """ Alternative, less verbose, printing method. """
+    print(chessboard.queens)
+
+def check(f, h) -> bool:
+    """ Checks if queens on fields f and h are attacking each other. """
+    if f[0] == h[0] :
+        return True
+    elif f[1] == h[1] :
+        return True
+    elif abs(f[1] - h[1]) == abs(f[0] - h[0]) :
+        return True
+    else :
+        return False
+
+def mono_solver(chessboard: ChessBoard, output) :
+    """ Actual, recurrent solving function - monoprocess """
+    try :
+        col = chessboard.queens.index(None) + 1
+    except ValueError :
+        output(chessboard)
+    else :
+        for f in chessboard.get_fields(col) :
+            chessboard.place_queen(f)
+            mono_solver(chessboard, output)
+            chessboard.place_queen(f)
+
+def multi_solver(chessboard: ChessBoard, output) :
+    """ Actual, recurrent solving function - multiprocess """
+    try :
+        col = chessboard.queens.index(None) + 1
+    except ValueError :
+        output(chessboard)
+    else :
+        with ProcessPoolExecutor() as executor :
+            for f in chessboard.get_fields(col) :
+                chessboard.place_queen(f)
+                executor.submit(mono_solver, deepcopy(chessboard), output)
+                chessboard.place_queen(f)
+
+def convert_args(queens: list | None, dim: int) -> list | None:
+    """ Converts queens arrangement provided from commandline, to proper format. """
+    if queens is None :
+        return None
+    result = []
+    for i in queens :
+        if i == 0 :
+            result.append(None)
+        elif i > dim :
+            raise ValueError(f"Row number {i} out of range, cannot be greater than dim={dim}")
+        elif i < 0 :
+            raise ValueError(f"Row number cannot be negative, but {i} was given.")
+        else :
+            result.append(i)
+    return result
+
+    
+if __name__ == '__main__' :
+    from argparse import ArgumentParser
+    
+    parser = ArgumentParser(
+            description='Calculate solutions of the n-queens problem.',
+            epilog="""
+        Either use interactive mode (python -i chessboard.py),
+        or provide dimension of chessboard to solve.
+        Use help(ChessBoard) for help in interactive mode.
+        """)
+    parser.add_argument('-v', '--verbose', action='store_true', help='Produce verbose output.')
+    parser.add_argument('-m', '--multi', action='store_true', help='Toggle multiprocessing.')
+    parser.add_argument('-d', '--dim', type=int, help="Chessboard's dimension.")
+    parser.add_argument('-q', '--queens', nargs='*', type=int, help='Provide initial arrangement of queens on the chessboard: c1 c2 ... cn, where c1 indicates position in first column. When column is empty use 0.')
+    args = parser.parse_args()
+
+
+    if isinstance(args.dim, int) :
+
+        ChessBoard(args.dim, convert_args(args.queens, args.dim)).solutions(args.multi, args.verbose)
+
+    else :
+        parser.print_help()
