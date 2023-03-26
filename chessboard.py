abf629807c639eb8a6f7c5f108bfc573bb7b9ded
Feasuro <feasuro@proton.me>
2023-03-26 19:24:53 +0200

    add main loop

--- chessboard.py
+++ chessboard.py
@@ -8,45 +8,6 @@ from collections.abc import MutableSequence
 import solver
 
 
-def convert_args(queens: list | None, dim: int) -> list | None:
-    """ Converts queens arrangement provided from commandline, to proper format. """
-    if queens is None :
-        return None
-    result = []
-    for i in queens :
-        if i == 0 :
-            result.append(None)
-        elif i > dim :
-            raise ValueError(f"Row number {i} out of range, cannot be greater than dim={dim}")
-        elif i < 0 :
-            raise ValueError(f"Row number cannot be negative, but {i} was given.")
-        else :
-            result.append(i)
-    return result
-
-class Queens(MutableSequence) :
-    def __init__(self, iterable) :
-        self.data = [int(item) for item in iterable]
-
-    def __setitem__(self, index, item) :
-        super().__setitem__(index, int(item))
-
-    def __getitem__(self, index) :
-        print(item)
-
-    def __delitem__(self, index) :
-        super().__setitem__(index, None)
-        
-    def __len__(self, index, item) :
-        super().__len__()
-    
-    def insert(self, index, item) :
-        pass
-
-    @property
-    def dim(self) :
-        return len(self)
-
 class ChessBoard :       
     """ Class implements the N Queens problem.
 
@@ -150,11 +111,10 @@ class ChessBoard :
                 print('Cannot place Queen here!')
                 return False
         else :
-            raise ValueError('Incorrect field coordinates (out of range?)')
+            raise ValueError('Incorrect field coordinates - out of range?')
 
     def from_list(self, queens):
         """ Populates board's initial values when queens arrangement is given to the constructor """
-        #for field in zip( range(1, self.dim+1), queens, strict=True ) :
         for field in enumerate(queens, start=1) :
             if field[1] is None :
                 continue
@@ -288,6 +248,46 @@ def input_check(dim, queens) :
             raise ValueError(f"Initial setup is invalid. Queens cannot attack each other")
     return True
 
+def get_command(x) :
+    if (y := x.upper()) in ['N', 'C', 'S', 'V', 'M', 'E'] :
+        return y
+    elif len(y := x.split()) == 2 :
+        return (int(y[0]), int(y[1]))
+    else :
+        raise ValueError("Command not recognized")
+
+def main() :
+    print("N - new chessboard")
+    print("C - clear chessboard")
+    print("x y - place/remove queen on field (x, y)")
+    print("S - print solutions")
+    print("m - toggle multiprocessing")
+    print("v - toggle verbose output")
+    print("E - exit program")
+    
+    while True :
+        try :
+            command = get_command(input())
+            print(args)
+        except ValueError :
+            print("Command not recognized")
+        else :
+            match command :
+                case 'E' :
+                    break
+                case 'V' :
+                    args.verbose = not args.verbose
+                case 'M' :
+                    args.multi = not args.multi
+                case 'C' :
+                    print("Clear")
+                case 'N' :
+                    print("New")
+                case 'S' :
+                    print("Solve")
+                case _ :
+                    print("Place a queen")
+        
 
 if __name__ == '__main__' :
     from argparse import ArgumentParser
@@ -300,7 +300,7 @@ if __name__ == '__main__' :
         Use help(ChessBoard) for help in interactive mode.
         """)
     parser.add_argument('-d', '--dim', type=dimension, help="Chessboard's dimension.")
-    parser.add_argument('-q', '--queens', nargs='*', type=coordinate, default=[], help='Provide initial arrangement of queens on the chessboard: c1 c2 ... cn, where c1 indicates position in first column. When column is empty use 0.')
+    parser.add_argument('-q', '--queens', nargs='*', type=coordinate, default=[], help="Initial arrangement of queens on the chessboard: c1 c2 ... cn, where cn is row number in n'th column. For empty column use N or 0.")
     parser.add_argument('-v', '--verbose', action='store_true', help='Produce verbose output.')
     parser.add_argument('-m', '--multi', action='store_true', help='Toggle multiprocessing.')
     args = parser.parse_args()
@@ -312,11 +312,11 @@ if __name__ == '__main__' :
         runtime = time.perf_counter() - start
 
     else :
-        while True :
+        main()
+        """while True :
             try :
-                args.dim = int(input("Provide chessboard's dimension: "))
-                if args.dim < 0 :
-                    raise ValueError
+                args.dim = dimension(input("Provide chessboard's dimension: "))
+                print(args, type(args))
                 break
             except ValueError :
-                print("Provide a non-negative integer!")
+                print("Provide a non-negative integer!")"""
