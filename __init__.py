d172a4d290091d0d47120108c2874116bf311244
Feasuro <feasuro@protonmail.com>
2024-10-04 21:39:46 +0200

    Gui translation

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 __all__ = [
     'solver',
     'chessboard',
-    'chessboard-qtgui'
+    'chessboard-qtgui',
+    'resources'
 ]
---
+++ resources/queens_pl.ts
@@ -0,0 +1,87 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>chessboard</name>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="105"/>
+        <source>Forbidden</source>
+        <translation>Niedozwolone</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="105"/>
+        <source>Cannot place Queen here!</source>
+        <translation>Nie można tutaj postawić Hetmana!</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="126"/>
+        <source>N-Queens solver</source>
+        <translation>Rozwiązywacz problemu N-Hetmanów</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="148"/>
+        <source>Solutions</source>
+        <translation>Rozwiązania</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="156"/>
+        <source>new</source>
+        <translation>nowa</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="156"/>
+        <source>clear</source>
+        <translation>wyczyść</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="156"/>
+        <source>solve</source>
+        <translation>rozwiąż</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="156"/>
+        <source>cancel</source>
+        <translation>anuluj</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="165"/>
+        <source>Multiprocess solving</source>
+        <translation>Wieloprocesowe rozwiązywanie</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="185"/>
+        <source>New Chessboard</source>
+        <translation>Nowa Szachownica</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="185"/>
+        <source>Provide new chessboard&apos;s dimension:</source>
+        <translation>Podaj wymiar nowej szachownicy:</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="210"/>
+        <source>Computation in progress</source>
+        <translation>Obliczenia w toku</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="222"/>
+        <source>Finished</source>
+        <translation>Zakończono</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="229"/>
+        <source>{} known solutions</source>
+        <translation>{} znanych rozwiązań</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="230"/>
+        <source>Solved</source>
+        <translation>Rozwiązano</translation>
+    </message>
+    <message>
+        <location filename="../chessboard-qtgui.py" line="230"/>
+        <source>Found {} solutions in {} seconds.</source>
+        <translation>Znaleziono {} rozwiązań w {} sekund.</translation>
+    </message>
+</context>
+</TS>
